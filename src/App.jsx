3d1416727edb06d068750b7e1a918b073d9fa904
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-12-01 23:39:14 +1100

    tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>Welcome to Health and Care</>;
+  const [darkMode, setDarkMode] = useState(false);
+  return (
+    <div className=" min-h-screen flex items-center justify-center bg-gray-400">
+      <div className="text-4xl font-bold text-blue-900">
+        Health care landing page
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/IntroSection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IntroSection = () => {
+  return <p>intro section</p>;
+};
+
+export default IntroSection;
